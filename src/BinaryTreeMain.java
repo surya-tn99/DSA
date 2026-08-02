e8141334ef501ff8530a553c6565313e22dfb185
surya-tn99 <balasurya.tn99@gmail.com>
2026-08-02 15:53:16 +0530

    feartures implemented till now are insert , diplay(inorder traversal) , search(BFS , DFS) .

--- src/BinaryTreeMain.java
+++ src/BinaryTreeMain.java
@@ -152,33 +152,33 @@ public class BinaryTreeMain {
         Scanner sc = new Scanner(System.in);
         BinarySearchTree bst = new BinarySearchTree();
         byte choice ;
-        bst.insert(100);
-        bst.insert(150);
-        bst.insert(125);
-        bst.insert(175);
-        bst.insert(50);
-        bst.insert(25);
-        bst.insert(75);
-        bst.inorderTraversal();
-        bst.bfs(175);
-        bst.dfs(175);
+        // bst.insert(100);
+        // bst.insert(150);
+        // bst.insert(125);
+        // bst.insert(175);
+        // bst.insert(50);
+        // bst.insert(25);
+        // bst.insert(75);
+        // bst.inorderTraversal();
+        // bst.bfs(175);
+        // bst.dfs(175);
 
-//        do{
-//
-//            choice = 0;
-//            System.out.print("\n ()_() \n\n0.Exit\n1.Insert Node \n2.Display Node \n3.Depth First Search\n4.Breath First Search\nur Choice : ");
-//            choice = sc.nextByte();
-//            switch (choice){
-//                case 1 -> bst.insert(sc.nextInt());
-//                case 2 -> bst.inorderTraversal();
-//                case 3 -> bst.dfs(sc.nextInt());
-//                case 4 -> bst.bfs(sc.nextInt());
-//                default -> {
-//                    System.out.println("\n Bye Bye (^) + (^)");
-//                    choice = 0;
-//                }
-//            }
-//        }while(choice != 0 );
+       do{
+
+           choice = 0;
+           System.out.print("\n ()_() \n\n0.Exit\n1.Insert Node \n2.Display Node \n3.Depth First Search\n4.Breath First Search\nur Choice : ");
+           choice = sc.nextByte();
+           switch (choice){
+               case 1 -> bst.insert(sc.nextInt());
+               case 2 -> bst.inorderTraversal();
+               case 3 -> bst.dfs(sc.nextInt());
+               case 4 -> bst.bfs(sc.nextInt());
+               default -> {
+                   System.out.println("\n Bye Bye (^) + (^)");
+                   choice = 0;
+               }
+           }
+       }while(choice != 0 );
         sc.close();
         return;
     }
