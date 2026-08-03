2c3c4f6cd1be4d3532e4de12c5bfe0894c1c8ed8
surya-tn99 <balasurya.tn99@gmail.com>
2026-08-03 20:09:58 +0530

    added delete option on menu

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         BinarySearchTree bst = new BinarySearchTree();
         byte choice ;
-        bst.insert(100);
-        bst.insert(150);
-        bst.insert(125);
-        bst.insert(175);
-        bst.insert(50);
-        bst.insert(25);
-        bst.insert(75);
-        bst.inorderTraversal();
+//        bst.insert(100);
+//        bst.insert(150);
+//        bst.insert(125);
+//        bst.insert(175);
+//        bst.insert(50);
+//        bst.insert(25);
+//        bst.insert(75);
+//        bst.inorderTraversal();
         // bst.bfs(175);
         // bst.dfs(175);
-        bst.delete(150);
+//        bst.delete(150);
 
         bst.inorderTraversal();
         
@@ -26,13 +26,14 @@ public class Main {
         do{
 
             choice = 0;
-            System.out.print("\n ()_() \n\n0.Exit\n1.Insert Node \n2.Display Node \n3.Depth First Search\n4.Breath First Search\nur Choice : ");
+            System.out.print("\n ()_() \n\n0.Exit\n1.Insert Node \n2.Display Node \n3.Depth First Search\n4.Breath First Search\n5.Delete\nur Choice : ");
             choice = sc.nextByte();
             switch (choice){
                 case 1 -> bst.insert(sc.nextInt());
                 case 2 -> bst.inorderTraversal();
                 case 3 -> bst.dfs(sc.nextInt());
                 case 4 -> bst.bfs(sc.nextInt());
+                case 5 -> bst.delete(sc.nextInt());
                 default -> {
                     System.out.println("\n Bye Bye (^) + (^)");
                     choice = 0;
